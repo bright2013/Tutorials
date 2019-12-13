f2dc9f9ebf09b522004571f43f16df2938ec1503
Paul Huang <huangbaorong@hzgit.com>
2019-12-13 10:20:32 +0800

    Initial commit

---
+++ src/main/java/org/example/TestTemplate.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C), 2019-2019, 软件卓越人才班
+ * FileName: TestTemplate
+ * Author:   Paul Huang
+ * Date:     2019/12/13 9:57
+ * Description: 测试模板的效果
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package org.example;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈测试模板的效果〉
+ *
+ * @author Paul Huang
+ * @create 2019/12/13
+ * @since 1.0.0
+ */
+public class TestTemplate {
+
+
+    /**
+     * 功能描述: <br>
+     * 〈〉
+     *
+     * @params: [test]
+     * @return: void
+     * @since: 1.0.0
+     * @Author: Paul Huang
+     * @Date: 2019/12/13 10:13
+     */
+    public void test(String test){
+
+
+
+    }
+
+}
